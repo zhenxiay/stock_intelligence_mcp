d583dec1798649284b2ee0dee8b93cd8b1b3349b
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-08-03 14:36:37 +0200

    Fixed format issues

--- src/utils/input_preprocessor.py
+++ src/utils/input_preprocessor.py
@@ -9,15 +9,15 @@ def preprocess_rsi_input(input) -> int:
     '''
     Preprocess the RSI input value to ensure it is an integer.
     '''
-   try:
-       if isinstance(input, str):
+    try:
+        if isinstance(input, str):
            # Extract digits from the string
-           match = re.search(r'\d+', input)
-           if match:
-               return int(match.group())
-       elif isinstance(input, int):
-           return input
-   except ValueError as e:
-      print(f"ValueError: {e}")
-   except Exception as e:
-      print(f"A non ValueError occured: {e}")
+            match = re.search(r'\d+', input)
+            if match:
+                return int(match.group())
+        elif isinstance(input, int):
+            return input
+    except ValueError as e:
+        print(f"ValueError: {e}")
+    except Exception as e:
+        print(f"A non ValueError occured: {e}")
