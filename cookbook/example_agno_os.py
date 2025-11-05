d9aab938a495c782209ce9b6550eef717256ebb6
Yu Zhenxiao (MA/BDO72) <Zhenxiao.Yu@de.bosch.com>
2025-11-05 16:21:00 +0100

    Add cookbook file to demostrate mcp server functionality with agno os

---
+++ cookbook/example_agno_os.py
@@ -0,0 +1,80 @@
+'''
+This is an example to demonstrate the functionality of the MCP server with Agno agent.
+'''
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+os.environ["NO_PROXY"] = "localhost, 127.0.0.1"
+os.environ["no_proxy"] = "localhost, 127.0.0.1"
+
+import sys
+# Add src to path to import our modules
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
+from agno.agent import Agent
+from agno.tools.yfinance import YFinanceTools
+from agno.tools.mcp import MCPTools
+from agno.models.openai.responses import OpenAIResponses
+
+
+async def run_agent(message: str) -> None:
+    '''
+    Run the financial analyst agent with the given message.
+    The agent is connected with the tools from the MCP server stock_intelligence_cmp.
+    Make sure that the MCP server is running before executing this script.
+    In this example, this server is running on localhost port 8008 with streamable-http transport.
+    '''
+
+    # Initialize and connect to the MCP server
+    try:
+        mcp_tools = MCPTools(
+            url="http://localhost:8008/mcp",
+            transport="streamable-http",
+            timeout_seconds=60
+            )
+        await mcp_tools.connect()
+    except Exception as e:
+        print(f"Error connecting to MCP server: {e}")
+        return
+
+    try:
+        agent = Agent(
+            model=OpenAIResponses(id="gpt-4.1"),
+            #tools=[YFinanceTools()],
+            tools=[mcp_tools],
+            description="You are an investment analyst that researches stock prices, analyst recommendations, and stock fundamentals.",
+            instructions=['''Format your response using markdown and use tables to display data where possible.
+                             Use the tools available ONLY to you to gather the necessary data to answer the user's query.
+                             If no relevant information can be found over the tools, respond with "No relevant information found."'''],
+            )
+    
+        # Run the agent
+        await agent.aprint_response(message, markdown=True, stream=True)
+
+    except Exception as e:
+        print(f"Error while running the agent: {e}")
+        return
+
+    finally:
+        # Always close the connection when done
+        await mcp_tools.close()
+    
+def create_message(ticker: str) -> str:
+    '''
+    Create prompt for the agent to analyze stock performance with a selected ticker.
+    The ticker name can either be the Symbol like (APPL or MSFT) or just the name of the company (Apple or Microsoft).
+    '''
+    return f'''
+          Provide a summary of the most recent stock performance for {ticker} in 2025.
+          Please include following key metrics:
+            - Closing price changes of last 14 days, 
+            - Analyst recommendations, 
+            - Relative Strenth Index (RSI)
+          Format the response using markdown and include tables where appropriate.
+          '''
+
+if __name__ == "__main__":
+    message = create_message('SNOW')
+    asyncio.run(run_agent(message))
