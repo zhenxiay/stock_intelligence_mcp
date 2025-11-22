b6dfbc2f2655e14f2a2ff1d56f4e3aa99c66c10f
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-11-22 16:38:08 +0100

    Update output_preprocessor.py - add new function which generates data in toon format

--- src/stock_intelligence_mcp/utils/output_preprocessor.py
+++ src/stock_intelligence_mcp/utils/output_preprocessor.py
@@ -3,6 +3,7 @@ Utility functions for preprocessing outputs in the MCP tool for the AI agent.
 '''
 
 import json
+from toon import encode
 
 def json_output(output):
     '''
@@ -12,4 +13,9 @@ def json_output(output):
         "content_type": "application/json",
         "content": output
     }, indent=2)
-    
+
+def toon_output(output):
+    '''
+    This function convert the output in toon format optimizes LLM token consumption
+    '''
+    return encode(output)
